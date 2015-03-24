86ea856124c93584cceb0d5bd409a09ae661d049
Steven Zeiler <zeiler.steven@gmail.com>
2015-03-24 16:19:53 -0700

    [FEATURE] RippledLogMonitor base class and Subclass

--- monitor.js
+++ monitor.js
@@ -1,8 +1,17 @@
+
 import RippledLogMonitor from './lib/rippled_log_monitor'
 
 const RIPPLED_LOG_PATH = process.env.RIPPLED_LOG_PATH || '/var/log/rippled/debug.log'
 
-let monitor = new RippledLogMonitor()
+class Monitor extends RippledLogMonitor {
+
+  // @override
+  onValidation(entry) {
+    console.log('NEW VALIDATION!', entry)
+  }
+}
+
+let monitor = new Monitor()
 
 monitor.monitorFile(RIPPLED_LOG_PATH)
 
