26e8415e086a6fa9e067886d73d0d04b49749e9a
Steven Zeiler <zeiler.steven@gmail.com>
2015-03-25 15:30:19 -0700

    [FEATURE] Record validations in Mongodb

--- monitor.js
+++ monitor.js
@@ -1,17 +1,30 @@
-
 import RippledLogMonitor from './lib/rippled_log_monitor'
+import Validation from './lib/validation'
+import mongo from './lib/mongoose'
 
 const RIPPLED_LOG_PATH = process.env.RIPPLED_LOG_PATH || '/var/log/rippled/debug.log'
 
-class Monitor extends RippledLogMonitor {
-
+class MongoValidationLogger extends RippledLogMonitor {
   // @override
   onValidation(entry) {
-    console.log('NEW VALIDATION!', entry)
+    Validation.create(entry, (error, record) => {
+      if (error) {
+        return console.error('mongo create error', error)
+      }
+      console.log('saved validation in mongo', record)
+    })
   }
 }
 
-let monitor = new Monitor()
+let monitor = new MongoValidationLogger()
 
-monitor.monitorFile(RIPPLED_LOG_PATH)
+mongo.connection.once('open', callback => {
+  console.log('mongodb connected')
+  monitor.monitorFile(RIPPLED_LOG_PATH)
+})
+
+mongo.connection.on('error', error => {
+  console.error('mongo connection error:', error)
+  process.exit(0)
+})
 
