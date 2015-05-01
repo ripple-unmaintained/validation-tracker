36ca08ee0840180985dec1557c79c6e4907c794f
Steven Zeiler <zeiler.steven@gmail.com>
2015-05-01 13:10:53 -0700

    [FEATURE] Factor out Statsd, Hbase trackers

--- monitor.js
+++ monitor.js
@@ -1,53 +1,29 @@
-import RippledLogMonitor from './lib/rippled_log_monitor'
+import HbaseValidationTracker from './lib/hbase/validation_tracker'
+import StatsdValidationTracker from './lib/statsd/validation_tracker'
 import Hbase from 'hbase'
 import StatsD from 'node-statsd'
 import Promise from 'bluebird'
 import Moment from 'moment'
-
-Promise.promisifyAll(Hbase);
-
 const RIPPLED_LOG_PATH = process.env.RIPPLED_LOG_PATH || '/var/log/rippled/debug.log'
 
-class HbaseValidationLogger extends RippledLogMonitor {
-  constructor() {
-    this.client = Hbase({
-      host: process.env.HBASE_HOST,
-      port: process.env.HBASE_PORT
-    })
-    super()
-  }
-  // @override
-  onValidation(entry) {
-    let row = this.client.table(process.env.HBASE_TABLE)
-      // Use "public_key|ledger_hash" as row name
-      .row(`${entry.public_key}|${entry.hash}`)
+Promise.promisifyAll(Hbase)
 
-    row.putAsync('validation:public_key', entry.public_key)
-      .then(row.putAsync('validation:hash', entry.hash))
-      .then(row.putAsync('validation:datetime', Moment(new Date(entry.datetime)).format("YYYYMMDDHHmmss")))
-      .then(console.log('saved validation in hbase'))
-      .error(function(err) {
-        console.error('hbase put error', err)
-      })
-  }
-}
+let hbaseClient = Hbase({
+  host: process.env.HBASE_HOST,
+  port: process.env.HBASE_PORT
+})
 
-class StatsdValidationLogger extends RippledLogMonitor {
-  constructor() {
-    this.client = new StatsD({
-      host: process.env.STATSD_HOST,
-      port: process.env.STATSD_PORT
-    })
-    super()
-  }
-  // @override
-  onValidation(entry) {
-    this.client.increment(entry.public_key, 1)
-  }
-}
+let statsdClient = new StatsD({
+  host: process.env.STATSD_HOST,
+  port: process.env.STATSD_PORT
+})
 
-let monitor = new HbaseValidationLogger()
-let statsdMonitor = new StatsdValidationLogger()
+let trackers = [
+  new HbaseValidationTracker(hbaseClient),
+  new StatsdValidationTracker(statsdClient)
+]
 
-monitor.monitorFile(RIPPLED_LOG_PATH)
-statsdMonitor.monitorFile(RIPPLED_LOG_PATH)
+trackers.forEach(tracker => {
+  tracker.monitorFile(RIPPLED_LOG_PATH)
+})
+
